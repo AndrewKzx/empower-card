80ac272d5f45ceea59f1df8eeab52d81b1eed72e
LorenzoPixel <71122234+HealthyFryOption@users.noreply.github.com>
2023-06-17 10:03:54 +0800

    tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Welcome from "./components/Welcome";
+import "./css/main.css"
 
 export default function App() {
     return (
     <>
     <Welcome/>
-    <div>Standard Chatered Empower Card :D</div>
+    <div className="text-3xl font-bold underline">
+        Standard Chatered Empower Card :D
+    </div>
     </>
     );
-    }
+}
