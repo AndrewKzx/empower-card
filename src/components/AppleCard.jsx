099e3ee11bdf1afd2653c83680f06b39dd3b4fae
LorenzoPixel <71122234+HealthyFryOption@users.noreply.github.com>
2023-06-17 22:20:58 +0800

    a

--- src/components/AppleCard.jsx
+++ src/components/AppleCard.jsx
@@ -6,7 +6,7 @@ export default function Cards() {
 
  return (
     <div>
-        <div id='cardContainer' style={{width: "90vw",  height: "25vh", margin:"auto"}} className="bg-red-100 rounded-xl relative text-white shadow-2xl transition-transform transform hover:scale-110">
+        <div id='cardContainer' className="bg-red-100 rounded-xl relative text-white shadow-2xl transition-transform transform hover:scale-110">
             <img id='card' className="relative object-cover w-full h-full rounded-xl" src={applePayCard} />
         </div>
     </div>
