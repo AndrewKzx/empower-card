dc463e2a2830546b57c735212d0190d326011320
LorenzoPixel <71122234+HealthyFryOption@users.noreply.github.com>
2023-06-17 22:19:52 +0800

    a

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -5,34 +5,32 @@ import standardCharterLogo from "../img/standardCharterLogo.png"
 export default function Cards() {
 
  return (
-    <div>
-        <div className="w-45 h-56 bg-red-100 rounded-xl relative text-white shadow-2xl transition-transform transform hover:scale-110">
-            <img id='card' className="relative object-cover w-full h-full rounded-xl" src="https://i.imgur.com/kGkSg1v.png" />
-            <div className="w-full px-8 absolute top-8">
-                <div className="flex justify-between">
-                    <div className="">
-                        <p className="font-light">
-                            Corporate Benefits Card
-                        </p>
-                    </div>
-                    <img className="w-20 h-14" src={standardCharterLogo} />
-                </div>
-                <div className="pt-1">
+    <div id="cardContainer" className="bg-red-100 rounded-xl relative text-white shadow-2xl transition-transform transform hover:scale-110">
+        <img id='card' className="relative object-cover w-full h-full rounded-xl" src="https://i.imgur.com/kGkSg1v.png" />
+        <div className="w-full px-8 absolute top-8">
+            <div className="flex justify-between">
+                <div className="">
                     <p className="font-light">
-                        Balance
+                        Corporate Benefits Card
+                    </p>
+                </div>
+                <img className="w-20 h-14" src={standardCharterLogo} />
+            </div>
+            <div className="pt-1">
+                <p className="font-light">
+                    Balance
+                </p>
+                <p className="font-medium">
+                    RM 654.09
+                </p>
+                <br></br>
+                <div className="flex justify-between">
+                    <p className="font-medium tracking-more-wider">
+                        ****  ****  ****  7632
                     </p>
                     <p className="font-medium">
-                        RM 654.09
+                        VISA
                     </p>
-                    <br></br>
-                    <div className="flex justify-between">
-                        <p className="font-medium tracking-more-wider">
-                            ****  ****  ****  7632
-                        </p>
-                        <p className="font-medium">
-                            VISA
-                        </p>
-                    </div>
                 </div>
             </div>
         </div>
