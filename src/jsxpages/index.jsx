a6810c24b33a689f8c575c518d6ee7d19ec591aa
LorenzoPixel <71122234+HealthyFryOption@users.noreply.github.com>
2023-06-17 17:00:27 +0800

    d

--- src/jsxpages/index.jsx
+++ src/jsxpages/index.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-<<<<<<< HEAD
+import Nabar2 from "../components/Navbar2";
+import Navbar from "../components/NavbarLogo";
+import CircularChart from "../components/CircularChart";
+import "../css/main.css"
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <>
@@ -11,20 +15,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       </div>
       <CircularChart/>
     </>
-=======
-import Navbar from "../components/NavbarLogo";
-import CircularChart from "../components/CircularChart";
-import "../css/main.css"
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-      <>
-        <Navbar/>
-        <div className="text-xl font-bold p-5">
-            Expense and Details main
-        </div>
-        <CircularChart/>
-      </>
->>>>>>> a15408eb475958520525b8779251e1cb01dcbcdd
   </React.StrictMode>,
 )
