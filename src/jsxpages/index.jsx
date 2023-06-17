1830f9fb6bda543f94da78c98ba940edcb92b7d8
LorenzoPixel <71122234+HealthyFryOption@users.noreply.github.com>
2023-06-17 16:57:55 +0800

    d

--- src/jsxpages/index.jsx
+++ src/jsxpages/index.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <>
+      <Navbar/>
+      <div className="text-xl font-bold p-5">
+          Expense and Benefits Details
+      </div>
+      <CircularChart/>
+    </>
   </React.StrictMode>,
 )
