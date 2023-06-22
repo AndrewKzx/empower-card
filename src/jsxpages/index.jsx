179c3c536c23f89d69aa2d400c430ccce785eff1
Knazran <khairulnazran94@gmail.com>
2023-06-23 00:12:25 +0800

    feat: added quick action

--- src/jsxpages/index.jsx
+++ src/jsxpages/index.jsx
@@ -13,6 +13,12 @@ import "../css/index.css"
 import "../css/tailwind.css"
 import RadialBarChart from '../components/RadialBarChart';
 
+import {
+  NoSymbolIcon,
+  CreditCardIcon,
+  FolderPlusIcon
+} from "@heroicons/react/24/solid";
+
 const chartData = [32.1]
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -21,13 +27,28 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Navbar />
       <br></br>
       <Cards />
-      <a className="applePay" href="./src/pages/ApplePay.html">
-        <img src={folder} alt="Folder" width="10%" />
-        <p>Add your Card to your Wallet for Apple Pay</p>
+      <div className='flex w-full px-8 my-6 items-center justify-center'>
+        <div className='flex flex-col items-center justify-center w-1/3'>
+          <CreditCardIcon className="h-8 w-8 text-[#6154FE]" />
+          <p className='my-2 text-sm text-[#2B69F5]'>Card Details</p>
+        </div>
+        <div className='flex flex-col items-center justify-center w-1/3'>
+          <NoSymbolIcon className="h-8 w-8 text-[#6154FE]" />
+          <p className='my-2 text-sm text-[#2B69F5]'>Block Card</p>
+        </div>
+        <a href="./src/pages/RequestPage.html" className='flex flex-col items-center justify-center w-1/3'>
+          <FolderPlusIcon className="h-8 w-8 text-[#6154FE]" />
+          <p className='my-2 text-xs text-[#2B69F5]'>Request Increase</p>
+        </a>
+        
+      </div>
+      <a className='flex bg-black mx-8 py-2 px-4 rounded-xl' href="./src/pages/ApplePay.html">
+        <img src={folder} alt="Folder" width="8%" />
+        <p className='text-xs text-white ml-4'>Add your Card to your Wallet for Apple Pay</p>
       </a>
 
 
-      <div className="text-xl font-bold p-5">
+      <div className="mt-4 text-lg font-bold p-5">
         Expense and Benefits Overview
       </div>
       <div className='flex px-4'>
@@ -56,37 +77,37 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         </button>
       </div>
 
-      <div className="text-xl font-bold p-4">
+      <div className="text-lg font-bold p-4">
         Recent Transactions
       </div>
 
       <div className='px-4'>
-      <div className="flex flex-col space-y-6">
-        <div className="flex">
-          <div className="flex flex-col w-3/4">
-            <div>GRAB RIDES-EC</div>
-            <div className="text-xs text-[#949494]">Taxi & Limousines</div>
+        <div className="flex flex-col space-y-6">
+          <div className="flex">
+            <div className="flex flex-col w-3/4">
+              <div>GRAB RIDES-EC</div>
+              <div className="text-xs text-[#949494]">Taxi & Limousines</div>
 
+            </div>
+            <div className="w-1/4 text-[#F24E30]">-RM 25.40</div>
           </div>
-          <div className="w-1/4 text-[#F24E30]">-RM 25.40</div>
-        </div>
-        <div className="flex">
-          <div className="flex flex-col w-3/4">
-            <div>Anytime Fitness</div>
-            <div className="text-xs text-[#949494]">Health</div>
+          <div className="flex">
+            <div className="flex flex-col w-3/4">
+              <div>Anytime Fitness</div>
+              <div className="text-xs text-[#949494]">Health</div>
 
+            </div>
+            <div className="w-1/4 text-[#F24E30]">-RM 190.00</div>
           </div>
-          <div className="w-1/4 text-[#F24E30]">-RM 190.00</div>
-        </div>
-        <div className="flex">
-          <div className="flex flex-col w-3/4">
-            <div>Tony Roma's</div>
-            <div className="text-xs text-[#949494]">Food and Beverages</div>
+          <div className="flex">
+            <div className="flex flex-col w-3/4">
+              <div>Tony Roma's</div>
+              <div className="text-xs text-[#949494]">Food and Beverages</div>
 
+            </div>
+            <div className="w-1/4 text-[#F24E30]">-RM 98.10</div>
           </div>
-          <div className="w-1/4 text-[#F24E30]">-RM 98.10</div>
         </div>
-      </div>
       </div>
 
       <div className='flex justify-end my-4 px-4 mb-24'>
