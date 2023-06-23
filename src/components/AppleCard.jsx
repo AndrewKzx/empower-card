23ae64a50f5e2a531cb1809ba5b0c2a2bb59fd6d
Knazran <khairulnazran94@gmail.com>
2023-06-23 21:36:37 +0800

    feat: used a real empowerCard

--- src/components/AppleCard.jsx
+++ src/components/AppleCard.jsx
@@ -1,14 +1,13 @@
 import React, { useState } from 'react';
 
-import applePayCard from "../img/applePayCard.png"
+// import applePayCard from "../img/applePayCard.png"
+import applePayCard from "../img/empowerCard.png"
 
 export default function Cards() {
 
  return (
-    <div>
-        <div id='cardContainer' className="bg-red-100 rounded-xl relative text-white">
-            <img id='card' className="relative object-cover w-full h-full rounded-xl" src={applePayCard} />
-        </div>
+    <div className='flex justify-center items-center px-8'>
+       <img id='card' className="object-cover w-full h-full rounded-xl" src={applePayCard} />
     </div>
  );
 }
